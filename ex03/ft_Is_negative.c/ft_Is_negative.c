556bbfb4908828237e3ffba6f27dc11b1e473cd8
craig beyers <cbeyers@c5r5s9.wethinkcode.co.za>
2020-06-21 12:11:14 +0200

    day02

---
+++ ex03/ft_Is_negative.c/ft_Is_negative.c
@@ -0,0 +1,24 @@
+    #include <stdio.h>
+
+    int main()
+{    
+	int i = 'n'; 
+    putchar("enter a number: ");
+    gets("%i",n);
+    if(n<0)
+	{
+            putchar('N');
+    }
+    if(n>0)
+	{
+	        putchar('P');
+	}
+    if(n==0)
+	{
+	        putchar('P');
+	}	
+    return 0;
+} 
+
+
+
---
+++ ex04/ft_print_comb.c/ft_print_comb.c
@@ -0,0 +1,17 @@
+void ft_print_comb(void);
+
+ 
+{
+    int  i, j, k;
+    
+    for(i = 1; i <= 3; i++)
+	
+	    for(j = 1; j <= 3; j++)
+	   
+		    for(k = 1; k <= 3; k++)
+	
+			    putchar(i);
+				putchar(j);
+				putchar(k);
+		        return 0;
+} 
