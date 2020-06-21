6abe86dc9f9cfdb0c723e9a536e00f0709b35f64
craig beyers <cbeyers@c5r5s9.wethinkcode.co.za>
2020-06-21 14:06:13 +0200

    day02

--- ex05/ft_print_comb2.c/ft_print_comb2.c
+++ ex05/ft_print_comb2.c/ft_print_comb2.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+void ft_print_comb2(void)
 
-int main()
+
 {
-	int i;
-	for ( i = 0 ;i < 9
+	int i, j;
+    {
+	for(i = 00; i <= 99; i++)
+        {
+		for(j = 00; j <= 99; j++)
+		    {
+		    printf("%d, %d", i, j);
+		    }	
+		
+		}	
+	}
+  return 0;
+}	
